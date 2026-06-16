239b6e37edd6b95d29fedf98c575a38e59666c8b
Wojciech Borawski <w.borawski@cotynatostudio.pl>
2026-06-16 14:18:07 +0200

    Model i panel admina

--- devboard/admin.py
+++ devboard/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Project, Task, Comment
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner')
+    search_fields = ('name', 'owner')
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'assignee', 'status', 'priority', 'due_date')
+    search_fields = ('title','assignee', 'status')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('task', 'author')
+    search_fields = ('task', 'author')

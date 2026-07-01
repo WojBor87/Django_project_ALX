e99ad52dc8f8554c63b89cc27beca0e67fc04d60
Wojciech Borawski <w.borawski@cotynatostudio.pl>
2026-07-01 16:37:26 +0200

    zmiany w env

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -16,12 +16,8 @@ ALLOWED_HOSTS = [HOSTS]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'production': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
 }
